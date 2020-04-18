199093702fe2f7599cd1d57a20d8ec1f7dbaaf86
Alvaro Plata <ad.plata@uniandes.edu.co>
2020-04-17 20:58:24 -0500

    Joystick y landscape mode en ControlPad

--- lib/controlPad.dart
+++ lib/controlPad.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class ControlPad extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]);
     return Scaffold(
-      body: Center(
-        child: Text('Aquí van todos los widgets'),
-      ),
+      body: Center(child: Text('hi')),
     );
   }
 }
